5c8ef39cc3569c53f0f7b45d5f92fbb9d1a754e5
lukebricknell <lukecbricknell@gmail.com>
2023-01-18 12:23:53 +0200

    Added privacy policy and changed year

---
+++ LL Website/public/script.js
@@ -0,0 +1,23 @@
+const phoneMenu = document.querySelector("#phone-menu");
+const menuButton = document.querySelector("#menu-btn");
+
+menuButton.addEventListener("click", function () {
+  if (phoneMenu.classList.contains("hidden")) {
+    phoneMenu.classList.remove("hidden");
+    phoneMenu.classList.add("flex-col");
+  } else if (!phoneMenu.classList.contains("hidden")) {
+    phoneMenu.classList.remove("flex-col");
+    phoneMenu.classList.add("hidden");
+  }
+});
+
+document.addEventListener("keydown", function (e) {
+  if (!phoneMenu.classList.contains("hidden") && e.key == "Escape") {
+    phoneMenu.classList.remove("flex");
+    phoneMenu.classList.add("hidden");
+  }
+});
+
+const today = new Date();
+const year = today.getFullYear();
+document.getElementById("year").textContent = year;
